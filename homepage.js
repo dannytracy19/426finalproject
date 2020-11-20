4fdfd4a73954defff5609283e8e8acd5dd04598d
Clayton Saunders <claytonsaunders@Claytons-MBP.lan>
2020-11-20 16:28:15 -0500

    adding_map

--- homepage.js
+++ homepage.js
@@ -1,3 +1,7 @@
+$(function() {
+    $(`#meetup`).on("click", handleMeetup)
+});
+
 const handleMeetup = function(event){
     console.log("something");
     event.preventDefault();
@@ -6,7 +10,18 @@ const handleMeetup = function(event){
     let meettype = $('input:radio[name=meetingplace]:checked').val();
     let stars = $('input:radio[name=stars]:checked').val();
     let price = $('input:radio[name=price]:checked').val();
- 
+
+    let map = $("<div/>", {html: `<div id="meet-map"></div>
+                            <div id="right-panel">
+                            <h2>Results</h2>
+                            <ul id="places"></ul>
+                            <button id="more">More results</button>
+                            </div>`});
+
+    $('#setup-meet').replaceWith(map);
+
+    handleCreateMap();
+
     //let preferences = 'meat'
 
     const result = axios({
@@ -26,14 +41,75 @@ const handleMeetup = function(event){
     
 }
 
+const handleCreateMap = function(){
+    initMap();
+}
 
+function initMap() {
+    // Create the map.
+    const chapel_hill = { lat: 35.9132, lng: -79.0558 };
+    const map = new google.maps.Map(document.getElementById("meet-map"), {
+        center: chapel_hill,
+        zoom: 15,
+    });
 
+    // Create the places service.
+    const service = new google.maps.places.PlacesService(map);
+    let getNextPage;
+    const moreButton = document.getElementById("more");
+  
+    moreButton.onclick = function () {
+        moreButton.disabled = true;
+    
+        if (getNextPage) {
+            getNextPage();
+        }
+    };
+    // Perform a nearby search.
+    service.nearbySearch(
+        { location: chapel_hill, radius: 400, type: "store" },
+        (results, status, pagination) => {
+            if (status !== "OK") return;
+            createMarkers(results, map);
+            moreButton.disabled = !pagination.hasNextPage;
+    
+            if (pagination.hasNextPage) {
+            getNextPage = pagination.nextPage;
+            }
+        }
+    );
+}
 
+function createMarkers(places, map) {
+    const bounds = new google.maps.LatLngBounds();
+    const placesList = document.getElementById("places");
+
+    for (let i = 0, place; (place = places[i]); i++) {
+        const image = {
+            url: place.icon,
+            size: new google.maps.Size(20, 20),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(10, 20),
+            scaledSize: new google.maps.Size(25, 25),
+        };
+        new google.maps.Marker({
+            map,
+            icon: image,
+            title: place.name,
+            position: place.geometry.location,
+        });
+        const li = document.createElement("li");
+        li.textContent = place.name;
+        placesList.appendChild(li);
+        bounds.extend(place.geometry.location);
+    }
+    map.fitBounds(bounds);
+}
 
 $(function() {
-    $(`.meetup`).on("click", handleMeetup)
+    $(`#li`).on('click', handleMeetingPlace)
+})
 
-
-
-
-});
+const handleMeetingPlace = function() {
+    console.log("choosing");
+}
